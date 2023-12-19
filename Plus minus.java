09ae014678cc62a11455a4c3bbb981d770577079
SJDinesh99 <154301875+SJDinesh99@users.noreply.github.com>
2023-12-18 22:48:11 -0800

    Update Plus minus.java

--- Plus minus.java
+++ Plus minus.java
@@ -39,3 +39,14 @@ class Solution
         System.out.println(zeroRatio);
     }
 }
+
+//Sample Input
+STDIN           Function
+-----           --------
+6               arr[] size n = 6
+-4 3 -9 0 4 1   arr = [-4, 3, -9, 0, 4, 1]
+
+//Sample Output
+0.500000
+0.333333
+0.166667
