66d30eed1767cffb711aad1589506b3075a3c515
SJDinesh99 <154301875+SJDinesh99@users.noreply.github.com>
2023-12-18 22:44:22 -0800

    Update Intro to tutorial challenges.java

--- Intro to tutorial challenges.java
+++ Intro to tutorial challenges.java
@@ -24,3 +24,13 @@ class Solution
         }
     }
 }
+
+//Sample Input 
+STDIN           Function
+-----           --------
+4               V = 4
+6               arr[] size n = 6 (not passed, see function description parameters)
+1 4 5 7 9 12    arr = [1, 4, 5, 7, 9, 12]
+
+//Sample Output 
+1
